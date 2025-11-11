2862c8f0ba0cfa782a5df000fe1a0307c6b166e7
Gabriel Deva Caniyago <111201911994@mhs.dinus.ac.id>
2025-11-11 18:35:27 +0700

    tugas kedua

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = [
+            [
+                'nama' => 'admin',
+                'email' => 'admin@gmail.com',
+                'password' => Hash::make('admin'),
+                'role' => 'admin',
+            ],
+            [
+                'nama' => 'Dokter',
+                'email' => 'dokter@gmail.com',
+                'password' => Hash::make('dokter'),
+                'role' => 'dokter',
+            ],
+        ];
+        foreach ($users as $user) {
+            User::firstOrCreate(
+                ['email' => $user['email']], // cek berdasarkan email
+                $user
+            );
+        }
+    }
+}
